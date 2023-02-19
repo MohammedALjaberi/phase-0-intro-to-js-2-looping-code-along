49b9c4097ea4fa46b34d499cdb7ee4ee866ee497
Mohammed Al-jaberi <aljabri606@gmail.com>
2023-02-19 21:10:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+
+// function writeCards(name,event){
+// let Text = [] ; 
+// for (let i =0 ; i < name.length ; i++){
+//     Text[i] = `Thank you, ${name} for the wonderful ${event} gift!`
+// }
+// return Text ; 
+// }
+
+function countDown(number){
+    while(number>0){
+        console.log(number);
+        number--;
+    }
+    return number;
+}
+countDown(100);
